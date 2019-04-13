50c67803a9a303b1bc70b916fa6bf5ca04833d49
rockxie <rockxie@gmail.com>
2019-04-13 11:17:34 +0800

    fix

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,5 +1,5 @@
 var disqus_shortname = 'rockxie';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://rockxie.github.io';		//The absolute root address of your blog without '/' in the end.
+var hostbase = 'https://rockxie.github.io';		//The absolute root address of your blog without '/' in the end.
 var githubname = 'rockxie';						//Your username of GitHub.
 var repos = 'rockxie.github.io';					//Name of repository that contains your blog.
 var sitetitle = '坚如磐石 - rockxie\'s blog';						//Name of your blog.

926ae870778597692a8e70a0e7b0901b6da057f6
rockxie <rockxie@gmail.com>
2019-04-13 11:23:25 +0800

    fx

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -37,7 +37,7 @@ function main(){
 		(function() {
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
             dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            //(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
         })();
 	}
 	else{
@@ -181,7 +181,7 @@ function showlist(list){
         var s = document.createElement('script'); s.async = true;
 		s.type = 'text/javascript';
         s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+        //(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
     }());
 }
 
